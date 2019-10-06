afc97db65753dc772bf66518a114e68186d24180
Max Justicz <max@justi.cz>
2019-10-06 16:25:20 -0400

    make queues taller

--- test/integration/send_test.go
+++ test/integration/send_test.go
@@ -24,50 +24,52 @@ func TestSendNetBasic(t *testing.T) {
 	// Seed random
 	rand.Seed(time.Now().UnixNano())
 
-	form := url.Values{}
-	form.Set("wantmsg", "on")
+	for i := 0; i < 50; i++ {
+		form := url.Values{}
+		form.Set("wantmsg", "on")
 
-	// Pick a random number of nets
-	form.Set("netslider", fmt.Sprintf("%d", rand.Intn(maxNets)+1))
-	form.Set("netnumbox", "76")
-	form.Set("netquantitytype", "reg")
-	form.Set("displayname", "foo bar")
-	form.Set("linktype", "twitter")
-	form.Set("twittername", "foobar4u")
-	form.Set("msg", "baz biz")
+		// Pick a random number of nets
+		form.Set("netslider", fmt.Sprintf("%d", rand.Intn(maxNets)+1))
+		form.Set("netnumbox", "76")
+		form.Set("netquantitytype", "reg")
+		form.Set("displayname", "foo bar")
+		form.Set("linktype", "twitter")
+		form.Set("twittername", "foobar4u")
+		form.Set("msg", "baz biz")
 
-	// Generate random fake icon
-	var fakeIcon [300]byte
-	_, err := rand.Read(fakeIcon[:])
-	require.NoError(t, err)
-	form.Set("canvasdata", base64.StdEncoding.EncodeToString(fakeIcon[:]))
+		// Generate random fake icon
+		var fakeIcon [300]byte
+		_, err := rand.Read(fakeIcon[:])
+		require.NoError(t, err)
+		form.Set("canvasdata", base64.StdEncoding.EncodeToString(fakeIcon[:]))
 
-	// POST form
-	payURL := fmt.Sprintf("%s/pay", appDevHost)
-	resp, err := postForm(payURL, form, nil)
-	require.NoError(t, err)
+		// POST form
+		payURL := fmt.Sprintf("%s/pay", appDevHost)
+		resp, err := postForm(payURL, form, nil)
+		require.NoError(t, err)
 
-	// Must succeed
-	require.Equal(t, 200, resp.StatusCode)
+		// Must succeed
+		require.Equal(t, 200, resp.StatusCode)
 
-	// Decode response
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
+		// Decode response
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		require.NoError(t, err)
 
-	fr := common.PayFormResponse{}
-	err = json.Unmarshal(bodyBytes, &fr)
-	require.NoError(t, err)
+		fr := common.PayFormResponse{}
+		err = json.Unmarshal(bodyBytes, &fr)
+		require.NoError(t, err)
 
-	// Should contain token
-	require.NotEmpty(t, fr.StripeSession)
+		// Should contain token
+		require.NotEmpty(t, fr.StripeSession)
 
-	// Mark as paid (hit timeline webhook)
-	form = url.Values{}
-	form.Set("token", fr.StripeSession)
-	webhookURL := fmt.Sprintf("%s/api/webhook/%s/stripe", timelineDevHost, webhookDevSecret)
-	resp, err = postForm(webhookURL, form, nil)
-	require.NoError(t, err)
+		// Mark as paid (hit timeline webhook)
+		form = url.Values{}
+		form.Set("token", fr.StripeSession)
+		webhookURL := fmt.Sprintf("%s/api/webhook/%s/stripe", timelineDevHost, webhookDevSecret)
+		resp, err = postForm(webhookURL, form, nil)
+		require.NoError(t, err)
 
-	// Must succeed
-	require.Equal(t, 200, resp.StatusCode)
+		// Must succeed
+		require.Equal(t, 200, resp.StatusCode)
+	}
 }
